2b9f6e24996c3c0eb1cfc4736271a81a45860842
Maikol Soro <maikolsoro.z1998@gmail.com>
2021-06-14 19:08:53 -0600

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+ const App = () => {
+  return (
+    <View>
+      <Text>Navigation</Text>
+    </View>
+  )
+}
+
+export default App;
