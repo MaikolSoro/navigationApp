8b8611928cbb021ac3aa41011118b70578e51589
Maikol Soro <maikolsoro.z1998@gmail.com>
2021-06-15 21:37:35 -0600

    useSafeAreaInserts

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { View, Text} from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { styles } from '../theme/appTheme';
 
 export const SettingsScreen = () => {
+    
+   const insets =  useSafeAreaInsets();
     return (
-        <View>
-            <Text>Settings Screen</Text>
+        <View style={{ 
+                ...styles.globalMargin,
+                marginTop:insets.top + 20
+         }}>
+            <Text style={styles.title}>Settings Screen</Text>
         </View>
     )
 }
