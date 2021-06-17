6f5fcff9749dffc50a8b6e6086eea6aff25d0f61
Maikol Soro <maikolsoro.z1998@gmail.com>
2021-06-16 20:30:11 -0600

    Icons in the application - Installations

--- src/screens/TabOneScreen.tsx
+++ src/screens/TabOneScreen.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect} from 'react'
+import Icon from 'react-native-vector-icons/Ionicons';
+
 import { View, Text } from 'react-native';
+import { styles } from '../theme/appTheme';
+
 
 export const TabOneScreen = () => {
 
@@ -8,8 +12,11 @@ export const TabOneScreen = () => {
     }, []);
     
     return (
-        <View>
-            <Text>TabOneScreen</Text>
+        <View style={styles.globalMargin}>
+            <Text style={styles.title}>Iconos</Text>
+
+            <Icon name="airplane-outline" size={80} color="#900" />
+
         </View>
     )
 }
